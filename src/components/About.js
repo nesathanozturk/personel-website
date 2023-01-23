c744e6b4f1c6106210a679d84054d2129534f151
Neşathan Öztürk <macbookpro@MacBook-Pro.local>
2023-01-23 18:26:21 +0300

    style: edited text

--- src/components/About.js
+++ src/components/About.js
@@ -3,32 +3,29 @@ import { AiFillLinkedin, AiFillGithub, AiFillInstagram } from "react-icons/ai";
 
 function About() {
   return (
-    <section
-      id="home"
-      className="min-h-screen flex py-10 md:flex-row flex-col items-center"
-    >
-      <div className="flex-1 flex items-center justify-center h-full">
+    <section className="min-h-screen flex flex-col items-center py-10 md:flex-row">
+      <div className="h-full flex items-center justify-center flex-1">
         <img src="" alt="" className="md:w-11/12 h-full object-cover" />
       </div>
       <div className="flex-1">
-        <div className="md:text-left text-center">
-          <h1 className="md:text-5xl text-2xl md:leading-normal leading-10 text-white font-bold">
-            <span className="text-cyan-600 md:text-6xl text-5xl">
+        <div className="text-center md:text-left">
+          <h1 className="text-2xl text-white font-bold leading-10 md:text-5xl md:leading-normal">
+            <span className="text-5xl text-blue-600 md:text-6xl">
               Hello 👋
               <br />
             </span>
             My Name is <span>Neşathan</span>
           </h1>
-          <h4 className="md:text-2xl text-lg md:leading-normal leading-5 mt-4 font-bold text-gray-400">
+          <h4 className="mt-4 text-black text-lg font-bold leading-5 md:text-2xl md:leading-normal">
             Junior Frontend Developer
           </h4>
-          <button className="btn-primary mt-8">
+          <button className="flex items-center gap-2 py-3 px-6 mt-8 mx-auto bg-blue-600 text-white font-semibold rounded-md transition-colors hover:bg-blue-800 md:mx-0">
             <a href="mailto:nesathanozturk061@gmail.com">Contact Me</a>
           </button>
-          <div className="mt-8 text-5xl flex items-center md:justify-start justify-center gap-5 cursor-pointer">
-            <AiFillLinkedin />
-            <AiFillGithub />
-            <AiFillInstagram />
+          <div className="flex justify-center items-center gap-3 mt-8 text-5xl cursor-pointer md:justify-start">
+            <AiFillLinkedin className="text-blue-700" />
+            <AiFillGithub className="text-purple-800" />
+            <AiFillInstagram className="text-red-700" />
           </div>
         </div>
       </div>
