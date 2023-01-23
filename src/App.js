f5bd775738e05f6c0796547a4221a54c0ed81504
Neşathan Öztürk <macbookpro@MacBook-Pro.local>
2023-01-23 17:55:49 +0300

    style: edited appjs

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div>
+    <div className="box-border bg-gray-900 text-gray-100 font-Poppins scroll-smooth">
       <Navbar />
       <About />
       <Footer />
