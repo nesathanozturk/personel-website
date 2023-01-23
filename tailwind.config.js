7552f9a55d662e01369b30829a80cb46cd91d4f0
Neşathan Öztürk <macbookpro@MacBook-Pro.local>
2023-01-23 18:02:32 +0300

    feat: added font and darkMode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        Poppins: "Poppins",
+        poppins: ["Poppins", "sans-serif"],
       },
     },
   },
