624afa28b30ae2025cbe71e6b02505ff168798f2
Neşathan Öztürk <macbookpro@MacBook-Pro.local>
2023-01-23 18:11:25 +0300

    style: edited footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="p-4 text-black text-sm text-center dark:text-white">
+    <footer className="p-4 text-black text-sm text-center font-medium dark:text-white">
       Made with by ❤️ Neşathan Öztürk
     </footer>
   );
